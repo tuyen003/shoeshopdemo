78dccdffb911342e342da109f84ffc96a2d7eb86
tuyen003 <tuyencontact2001@gmail.com>
2022-07-02 12:11:42 +0700

    update

--- components/review_product.php
+++ components/review_product.php
@@ -3,10 +3,12 @@ require('../server/connect.php');
 // session_start();
 
 $message = array();
+session_start();
 
 $product_id = $_POST["product_id"];
 $user_id = $_POST["user_id"];
 $user_name = $_POST["user_name"];
+$user_email = $_SESSION['user_info']["user_email"];
 $content = $_POST["content"];
 $rate = $_POST["rate"];
 $date = date("Y-m-d");
@@ -14,8 +16,8 @@ $date = date("Y-m-d");
 if(!isset($_SESSION['user_info'])){
     $message['mess'] = "not_user";
 } else {
-    $sql = "INSERT INTO `rating_products`(`product_id`, `user_id`,`user_name`, `rate_content`, `rate_status`, `rate_date`)
-    VALUES ('{$product_id}','{$user_id}','{$user_name}','{$content}','{$rate}','{$date}')";
+    $sql = "INSERT INTO `rating_products`(`product_id`, `user_id`,`user_name`,`user_email`,`rate_content`, `rate_status`, `rate_date`)
+    VALUES ('{$product_id}','{$user_id}','{$user_name}','{$user_email}','{$content}','{$rate}','{$date}')";
     $result = mysqli_query($conn,$sql);
 
     if($result) {
