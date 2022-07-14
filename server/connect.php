d1b578bfd2a4c08f563038e91fce03caa1418f16
tuyen003 <tuyencontact2001@gmail.com>
2022-07-14 18:00:16 +0700

    update

--- server/connect.php
+++ server/connect.php
@@ -4,8 +4,8 @@
     // $password = 'mLeaXYzyiiW6e';
     // $db ='epiz_31937328_shoes';
 
-    // $conn = mysqli_connect($server,$user,$password,$db) or die("Kết nối thất bại");
+    $conn = mysqli_connect('sql309.epizy.com','epiz_31937328','mLeaXYzyiiW6e','epiz_31937328_shoes') or die("Kết nối thất bại");
     // $conn = mysqli_connect('localhost','root','','shoes') or die("Kết nối thất bại");
-    $conn = mysqli_connect('remotemysql.com','v1QylMCmMH','oLf6hZPY3M','v1QylMCmMH') or die("Kết nối thất bại");
+    // $conn = mysqli_connect('remotemysql.com','v1QylMCmMH','oLf6hZPY3M','v1QylMCmMH') or die("Kết nối thất bại");
 
 ?>
